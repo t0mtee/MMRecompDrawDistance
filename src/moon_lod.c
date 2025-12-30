5042c39c07cfa142399d5e392aef601b69d6593f
t0mtee <33041004+t0mtee@users.noreply.github.com>
2025-12-30 13:48:36 +0000

    Fix compatibility with SkipIt.

--- src/moon_lod.c
+++ src/moon_lod.c
@@ -9,6 +9,6 @@ RECOMP_PATCH void EnFall_LodMoon_Draw(Actor* thisx, PlayState* play) {
 
 // We need to make sure that whenever the game wants to load object_lodmoon into memory, it actually loads object_fall.
 // This is needed because otherwise EnFall_Moon_Draw tries to access gMoonDL, which isn't loaded into memory, and we get a segfault.
-RECOMP_CALLBACK("*", recomp_after_play_init) void after_play_init(PlayState* this) {
+RECOMP_HOOK("Main") void Main_Init(void* arg) {
     gObjectTable[OBJECT_LODMOON] = gObjectTable[OBJECT_FALL];
 }
